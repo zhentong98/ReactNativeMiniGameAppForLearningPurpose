44fd7069f2e1aec3ac1fe4b7b49740acf43923d1
Chen Tong <chentong@xover.com.my>
2024-01-06 21:48:17 +0800

    Update

--- App.js
+++ App.js
@@ -5,19 +5,30 @@ import {LinearGradient} from "expo-linear-gradient"
 import StartGameScreen from "./screens/StartGameScreen";
 import GameScreen from "./screens/GameScreen";
 import Colors from "./constants/colors";
+import GameOverScreen from "./screens/GameOverScreen";
 
 export default function App() {
 
     const [userNumber, setUserNumber] = useState();
+    const [gameIsOver, setGameIsOver] = useState(true);
 
     function pickedNumberHandler(pickedNumber) {
         setUserNumber(pickedNumber);
+        setGameIsOver(false);
+    }
+
+    function gameOverHandler() {
+        setGameIsOver(true);
     }
 
     let screen = <StartGameScreen onPickNumber={pickedNumberHandler}/>;
 
     if (userNumber) {
-        screen = <GameScreen/>;
+        screen = <GameScreen userNumber={userNumber} onGameOver={gameOverHandler}/>;
+    }
+
+    if (gameIsOver && userNumber) {
+        screen = <GameOverScreen />
     }
 
     return (
--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,12 +1,63 @@
-import {StyleSheet, Text, View} from "react-native";
+import {useState, useEffect} from "react";
+import {StyleSheet, Text, View, Alert} from "react-native";
 
-function GameScreen() {
+import NumberContainer from "../components/game/NumberContainer";
+import PrimaryButton from "../components/ui/PrimaryButton";
+import Title from "../components/ui/Title";
+
+function generateRandomBetween(min, max, exclude) {
+    const rndNum = Math.floor(Math.random() * (max - min)) + min;
+
+    if (rndNum === exclude) {
+        return generateRandomBetween(min, max, exclude);
+    } else {
+        return rndNum;
+    }
+}
+
+let minBoundary = 1;
+let maxBoundary = 100;
+
+function GameScreen({userNumber, onGameOver}) {
+
+    const initialGuess = generateRandomBetween(1, 100, userNumber);
+    const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
+    useEffect(() => {
+        if (currentGuess === userNumber) {
+            onGameOver();
+        }
+    }, [currentGuess, userNumber, onGameOver])
+
+    function nextGuessHandler(direction) { // direction => 'lower' or 'greater'
+
+        if ((direction === 'lower' && currentGuess < userNumber) || (direction === 'greater' && currentGuess > userNumber)) {
+            Alert.alert('Don\'t lie!', 'You know that this is wrong...', [{text: 'Sorry!', style: 'cancel'}]);
+            return;
+        }
+
+        if (direction === 'lower') {
+            maxBoundary = currentGuess;
+        } else {
+            minBoundary = currentGuess + 1;
+        }
+
+        const newRndNumber = generateRandomBetween(minBoundary, maxBoundary, currentGuess);
+        setCurrentGuess(newRndNumber);
+    }
+
     return (
         <View style={styles.screen}>
-            <Text style={styles.title}>Opponent's Guess</Text>
-            {/*GUESS*/}
+            <Title>Opponent's Guess</Title>
+            <NumberContainer>
+                {currentGuess}
+            </NumberContainer>
             <View>
                 <Text>Higher or lower?</Text>
+                <View>
+                    <PrimaryButton onPress={nextGuessHandler.bind(this, 'lower')}>+</PrimaryButton>
+                    <PrimaryButton onPress={nextGuessHandler.bind(this, 'greater')}>-</PrimaryButton>
+                </View>
             </View>
             <View>
                 {/*Log Rounds*/}
